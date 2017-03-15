e85be75d08d264ad6a40c4167ee3f42bb347379a
andrem37 <andrem37@gmail.com>
2017-03-15 17:31:39 -0600

    Update MIPS.asm

--- MIPS.asm
+++ MIPS.asm
@@ -46,9 +46,7 @@ section .data
 
 	; -------------------- LECTURA ROM.TXsT --------------------
 	file db "./ROM.txt", 0
-	len equ 2048
-	n db 0
-
+	len equ 10000
 	; -------------------- Imprimir registros --------------------
 	filename: db "myfile.txt",0
 
@@ -58,8 +56,8 @@ section .bss
 
 	; -------------------- LECTURA ROM.TXT --------------------
 	buffer: resb 2048
-	trama: resb 1
-
+	trama: resb 2048
+	pc: resb 2048
 	; -------------------- Reservación en memoria para registros MIPS --------------------
 	; De 4 bytes = 32 bits.
 	reg0: resb 4
@@ -115,59 +113,94 @@ _start:
 	printString sem, lsem
 	printString buscando, lbuscando
 
-; -------------------- ABRIR ROM.TXT --------------------
-	mov ebx, file ; name of the file
-	mov eax, 5
-	mov ecx, 0
-	int 80h
+;-----------------------LECTURA ROM.TXT--------------------------------
 
-	mov eax, 3
+
+
+;----------------ABRIR ROM.TXT---------------------
+	mov ebx, file ; name of the file  
+	mov eax, 5  
+	mov ecx, 0  
+	int 80h     
+
+	mov eax, 3  
 	mov ebx, eax
-	mov ecx, buffer
-	mov edx, len
-	int 80h
+	mov ecx, buffer 
+	mov edx, len    
+	int 80h 
 
-;-------------------- PASAR DE ASCII A ENTERO --------------------
+;-----------PASAR DE ASCII A ENTERO----------------
 	mov r8, [buffer]
 	mov rax, 0
 	mov rbx, 0
+	mov r15, 0
 
 loop1:
-	add rax, 11
+
+	inc rax
 
 	mov r8, [buffer+rax]
 	ASCIIaENTERO r8
 	inc rax
-
 	mov r9, [buffer+rax]
 	ASCIIaENTERO r9
 	inc rax
-
 	mov r10, [buffer+rax]
 	ASCIIaENTERO r10
 	inc rax
-
 	mov r11, [buffer+rax]
 	ASCIIaENTERO r11
 	inc rax
-
 	mov r12, [buffer+rax]
 	ASCIIaENTERO r12
 	inc rax
-
 	mov r13, [buffer+rax]
 	ASCIIaENTERO r13
 	inc rax
-
 	mov r14, [buffer+rax]
 	ASCIIaENTERO r14
 	inc rax
-
 	mov r15, [buffer+rax]
 	ASCIIaENTERO r15
 	inc rax
+;-------------GUARDAR PC EN MEMORIA------------------
+	HexPC r14,r15,rbx
+	inc rbx
+	HexPC r12,r13,rbx
+	inc rbx
+	HexPC r10,r11,rbx
+	inc rbx
+	HexPC r8,r9,rbx
+	inc rbx
 
-;-------------------- GUARDAR EN MEMORIA --------------------
+	SUB rbx, 4
+	add rax, 2
+
+	mov r8, [buffer+rax]
+	ASCIIaENTERO r8
+	inc rax
+	mov r9, [buffer+rax]
+	ASCIIaENTERO r9
+	inc rax
+	mov r10, [buffer+rax]
+	ASCIIaENTERO r10
+	inc rax
+	mov r11, [buffer+rax]
+	ASCIIaENTERO r11
+	inc rax
+	mov r12, [buffer+rax]
+	ASCIIaENTERO r12
+	inc rax
+	mov r13, [buffer+rax]
+	ASCIIaENTERO r13
+	inc rax
+	mov r14, [buffer+rax]
+	ASCIIaENTERO r14
+	inc rax
+	mov r15, [buffer+rax]
+	ASCIIaENTERO r15
+	inc rax
+;-------------GUARDAR EN MEMORIA------------------
 	HexMemoria r14,r15,rbx
 	inc rbx
 	HexMemoria r12,r13,rbx
@@ -178,28 +211,65 @@ loop1:
 	inc rbx
 
 loop2:
+
 	mov r8, [buffer+rax]
 	and r8, 11111111b
 
 	cmp r8, 0
-	je end
+	je finLectura
 
 	cmp r8, 10
 	je esEnter
 	inc rax
 	jmp loop2
 
-esEnter:
+	esEnter:
 	inc rax
 	mov r8, [buffer+rax]
 	and r8, 11111111b
 	cmp r8, 0
-	je end
+	je finLectura
 	cmp r8, 10
 	je esEnter
 	jmp loop1
 
-end:
+determinarPC:
+
+	; R15 registro PC
+
+	cmp ebx, 0
+	ja loopDeterminarPC
+	add r15, 4
+	mov eax, [trama+r15]
+	jmp inicio
+
+loopDeterminarPC:
+
+	mov eax, [pc+r15]
+	sub eax, ebx
+
+	cmp eax, 0x80000000
+	ja loopEsMayor
+
+	sub r15d, eax
+	mov eax, [trama+r15]
+	jmp inicio
+
+loopEsMayor:
+
+
+	add r8d, eax
+	not r8d
+	inc r8	
+	add r15, r8
+	mov eax, [trama+r15]
+	jmp inicio
+
+finLectura:
+	mov ebx, 0;
+
+inicio:
+
 	mov rax, eax
 	separarJ rax
 	jmp decode
