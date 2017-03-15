3db5b8307ccf63b5ded64483a95800bd8036527a
jvsqzj <jvsqzj@gmail.com>
2017-03-14 19:33:39 -0600

    Cambio de formato de numeros

--- ALU.asm
+++ ALU.asm
@@ -2,31 +2,31 @@
 	mov rsi, %1 ; "OP Code" de ALU. Se guarda en rsi.
 
 %%and:
-	cmp rsi, 0d ; 0d = AND.
+	cmp rsi, 0 ; 0d = AND.
 	jne %%or ; Si no es, pasa a siguiente opción.
 	and rax, rcx ; rax (rs) AND rcx (rt).
 	jmp %%result ; Concluida la operación, pasa a resultado.
 
 %%or:
-	cmp rsi, 1d ; 1d = OR.
+	cmp rsi, 1 ; 1d = OR.
 	jne %%add ; Si no es, pasa a siguiente opción.
 	or rax, rcx ; rax (rs) OR rcx (rt).
 	jmp %%result ; Concluida la operación, pasa a resultado.
 
 %%add:
-	cmp rsi, 2d ; 2d = ADD.
+	cmp rsi, 2 ; 2d = ADD.
 	jne %%substract ; Si no es, pasa a siguiente opción.
 	add rax, rcx ; rax (rs) + rcx (rt).
 	jmp %%result ; Concluida la operación, pasa a resultado.
 
 %%substract:
-	cmp rsi, 3d; 3d = SUB.
+	cmp rsi, 3; 3d = SUB.
 	jne %%set_on_less_than ; Si no es, pasa a siguiente opción.
 	sub rax, rcx ; rax (rs) - rcx (rt).
 	jmp %%result ; Concluida la operación, pasa a resultado.
 
 %%set_on_less_than:
-	cmp rsi, 4d; 4d = SLTU.
+	cmp rsi, 4; 4d = SLTU.
 	jne %%nor ; Si no es, pasa a siguiente opción.
 	cmp rcx, rax
 	jg %%true  ; Si rcx (rt) > rax(rs).
@@ -41,14 +41,14 @@
 		jmp %%end ; Se pasa a fin porque ya resultado está en rd.
 
 %%nor:
-	cmp rsi, 5d; 5d = NOR.
+	cmp rsi, 5; 5d = NOR.
 	jne %%multiply ; Si no es, pasa a siguiente opción.
 	or rax, rcx ; rax (rs) OR rcx (rt).
 	not rax ; NOT rax (rs).
 	jmp %%result ; Concluida la operación, pasa a resultado.
 
 %%multiply:
-	cmp rsi, 6d ; 6d = MULT.
+	cmp rsi, 6 ; 6d = MULT.
 	jne %%end ; Si no es, pasa a resultal sin hacer nada.
 	mul rcx ; rax (rs) * rcx (rt).
 	jmp %%result ; Concluida la operación, pasa a resultado.
@@ -67,7 +67,7 @@ section .text
 _start:
 	mov rax, 3
 	mov rcx, 4
-	alu 6d ; mult.
+	alu 6 ; mult.
 
 	mov rax, 60	; Se carga la llamada 60d (sys_exit) en rax.
 	mov rdi, 0	; En rdi se carga un 0.
